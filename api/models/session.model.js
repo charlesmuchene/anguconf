af1d41eafb1950c95ec71abc8a52dccb1dd2dca9
Charles Muchene <charlesmuchene@gmail.com>
2019-04-20 10:40:31 -0500

    Adds session schema validation

--- api/models/session.model.js
+++ api/models/session.model.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 
 const SessionSchema = mongoose.Schema(
 	{
-		name: String,
-		time: Date
+		title: {
+			min: 3,
+			type: String,
+			required: [ true, 'Session title cannot be empty' ]
+		},
+		content: {
+			type: String,
+			required: [ true, 'Session content cannot be empty' ]
+		},
+		time: {
+			type: Date,
+			required: [ true, 'Session data cannot be empty' ]
+		}
 	},
 	{
 		timestamps: true
