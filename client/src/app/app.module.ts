35fc7837a23ec2ad4a734bc165e132ce6653d942
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-20 12:28:06 -0500

    creating Homepage  nav bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-	declarations: [ AppComponent ],
+	declarations: [ AppComponent, LoginComponent, SignUpComponent ],
 	imports: [ BrowserModule, AppRoutingModule, MatToolbarModule, BrowserAnimationsModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
