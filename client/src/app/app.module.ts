ffcbc831f203019620a9313b84fbf2ac4867fd5a
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-20 14:33:16 -0500

    adding loging page and model

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+
+ 
 @NgModule({
 	declarations: [ AppComponent, LoginComponent, SignUpComponent ],
-	imports: [ BrowserModule, AppRoutingModule, MatToolbarModule, BrowserAnimationsModule ],
+	imports: [ BrowserModule, AppRoutingModule, MatToolbarModule, BrowserAnimationsModule,ReactiveFormsModule,MatFormFieldModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,19 +1,37 @@
+import { LoginModel } from './../models/login.models';
 import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <p>
-      login works!
-    </p>
-  `,
+  templateUrl:'/login.component.html', 
+
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: LoginModel = new LoginModel();
+  loginForm: FormGroup;
+  hide = true;
 
-  constructor() { }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      'email': [this.user.email, [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': [this.user.password, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ]]
+    });
+  }
+  onLoginSubmit(){
+    alert(this.user.email + ''+ this.user.password);
   }
 
 }
