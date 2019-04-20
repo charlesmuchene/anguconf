cd81684e5fb38f82667bd9e6dad16bdce4c34556
Charles Muchene <charlesmuchene@gmail.com>
2019-04-20 10:17:38 -0500

    Refactors app.js
    Adds session router
    Adds error handler

--- api/api.js
+++ api/api.js
@@ -1,15 +1,28 @@
+/// Dependencies
+
+// Modules
 require('dotenv').config({ debug: process.env.DEBUG });
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+
+// Routes
+const sessionRouter = require('./routes/session.routes');
+
+/// Configuration
 
 mongoose
 	.connect(process.env.DB_URI, { useNewUrlParser: true })
 	.then(() => console.log('Connected to db.'))
 	.catch((error) => console.log(`Error connecting to db: ${error.message}`));
 
+/// Setup
+
 // create express app
 const app = express();
+
+/// Middleware
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,14 +30,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// define a index route
+/// Routes
+
 app.get('/', (req, res) => {
 	res.json({
 		message: 'Anguconf. See you there!'
 	});
 });
 
-// listen for requests
-app.listen(1234, () => {
-	console.log('Server is listening on port 1234');
+app.use('/sessions', sessionRouter);
+
+app.use((request, response, next) => {
+	next(createError(404, "Couldn't find the page you're looking for"));
 });
+
+/// Errors
+
+app.use((error, request, response, next) => {
+	const code = error.status || 500;
+	response.status(code).json({
+		error: {
+			code: code,
+			message: error.message
+		}
+	});
+});
+
+/// Boot app
+
+// listen for requests
+app.listen(1234, () => console.log('Server is listening on port 1234'));
