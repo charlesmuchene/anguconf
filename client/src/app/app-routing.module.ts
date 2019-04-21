7efa516c465c601cc6851a96263ebe71717fc844
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-21 09:25:59 -0500

    adding route for sign-up component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'sign-up',component:SignUpComponent}
 ];
 
 @NgModule({
