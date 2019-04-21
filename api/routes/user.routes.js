5e16c11e1465d382ea545ee04651899255e77767
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-21 15:41:24 -0500

    user routes updated

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -5,61 +5,15 @@ const user=require('../controllers/user.controller.js');
 router.get('/',user.findAll);
 
 //Retrieve user with the given id
-//router.get('/:userId',user.findOne);
+router.get('/:userId',user.findOne);
 
 //create user
-router.post('/',user.update);
+router.post('/',user.create);
 
 //upate user with given id 
-//router.put('/:userId',user.update);
+router.put('/:userId',user.update);
 
 // Delete a user with the given id
-// router.delete('/:userId',user.delete);
-router.post('/login',(req,res)=>{
-    const email=req.body.email;
-    const password=req.body.password;
-    user.findOne({email})
-        .then(user=>{
-            if(!person){
-                return res.status(404).json({emailerror:'user not Found'})
-            }
-            else{
-                bcrypt.compare(password,user.password)
-                      .then(isCorrect=>{
-                          if(iscorrect){
-                              //res.json({success:'login success});
-                              const payload={
-                                  id:user.id,
-                                  email:user.email,
-                                  name:user.name
-                              };
-                              jsonwt.sign(
-                                  payload,
-                                  key.secret,
-                                  {
-                                      expiresIn:3600
-
-                                  },
-                                  (err,token)=>{
-                                      res.json({
-                                          success:true,
-                                          token:"Bearer" + token
-                                      });
-                                  }
-
-                                  );
-                                }
-                                else{
-                                    res.status(500).json({error:'internal error caused by bcrypt'});
-                                }
-                      })
-                      .catch(err=>console.log(err));
-            }
-        });
-});
-
-router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
-    res.send('authorized');
-})
+router.delete('/:userId',user.delete);
 
 module.exports=router;
