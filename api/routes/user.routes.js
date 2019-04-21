de0dc1f4b3795a8d3b9ddedddb1747f2bdc91422
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-21 13:37:16 -0500

    adding a post route for user post verb and sending token

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -15,5 +15,51 @@ router.post('/',user.update);
 
 // Delete a user with the given id
 // router.delete('/:userId',user.delete);
+router.post('/login',(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+    user.findOne({email})
+        .then(user=>{
+            if(!person){
+                return res.status(404).json({emailerror:'user not Found'})
+            }
+            else{
+                bcrypt.compare(password,user.password)
+                      .then(isCorrect=>{
+                          if(iscorrect){
+                              //res.json({success:'login success});
+                              const payload={
+                                  id:user.id,
+                                  email:user.email,
+                                  name:user.name
+                              };
+                              jsonwt.sign(
+                                  payload,
+                                  key.secret,
+                                  {
+                                      expiresIn:3600
+
+                                  },
+                                  (err,token)=>{
+                                      res.json({
+                                          success:true,
+                                          token:"Bearer" + token
+                                      });
+                                  }
+
+                                  );
+                                }
+                                else{
+                                    res.status(500).json({error:'internal error caused by bcrypt'});
+                                }
+                      })
+                      .catch(err=>console.log(err));
+            }
+        });
+});
+
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    res.send('authorized');
+})
 
 module.exports=router;
