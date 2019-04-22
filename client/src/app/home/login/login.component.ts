208c60046bbb202ff17d9ba59c3905bea9c67633
Charles Muchene <charlesmuchene@gmail.com>
2019-04-22 15:31:30 -0500

    Refactors login component

--- client/src/app/home/login/login.component.ts
+++ client/src/app/home/login/login.component.ts
@@ -3,7 +3,6 @@ import { LoginModel } from '../../models/login.models';
 import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-login',
@@ -13,21 +12,21 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 	user: LoginModel = new LoginModel();
 	private loginForm: FormGroup;
-	hide = true;
 
-	constructor(private formBuilder: FormBuilder, private router: Router, private homeService: HomeService) {}
+	constructor(private formBuilder: FormBuilder, private homeService: HomeService) {}
 
 	ngOnInit() {
+		this.createForm();
+	}
+
+	private createForm() {
 		this.loginForm = this.formBuilder.group({
-			email: [ this.user.email, [ Validators.required, Validators.email ] ],
-			password: [ this.user.password, [ Validators.required, Validators.minLength(6), Validators.maxLength(30) ] ]
+			email: [ 'charlo@internet.mwa', [ Validators.required, Validators.email ] ],
+			password: [ 'password', [ Validators.required, Validators.minLength(6), Validators.maxLength(30) ] ]
 		});
 	}
-	onLoginSubmit() {
-		alert(this.user.email + '' + this.user.password);
+	onSubmit() {
+		console.log(this.loginForm.valid);
 		// TODO Use homeservice to do api request to login user
-	}
-	show() {
-		this.router.navigateByUrl('sign-up');
 	}
 }
