0cc02e94082978afe553b88ad9a317386738e6ec
Redae Mengsteab <redmeha2013@gmail.com>
2019-04-23 15:10:26 -0500

    Adds role property

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,7 +5,8 @@ const userSchema = mongoose.Schema({
 	firstname: { type: String, required: true },
 	lastname: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true, unique: true }
+	password: { type: String, required: true },
+	role:{ type: String, enum: ['admin', 'user'], default: ['user'] }
 });
 
 userSchema.pre('save', function(next) {
