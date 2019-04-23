3e62b066abbd0837226034bb4e5be3d358c0fd1f
Charles Muchene <charlesmuchene@gmail.com>
2019-04-23 14:22:01 -0500

    Refactors service

--- client/src/app/sessions/sessions.service.ts
+++ client/src/app/sessions/sessions.service.ts
@@ -5,18 +5,25 @@ import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../store/store';
 import { Action } from '../models/action.model';
 import { createServerSessionsAction, createUserSessionsAction } from '../store/actions';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SessionsService {
+	private sessionUrl = 'http://localhost:1234/api/sessions';
+
 	constructor(private apiService: ApiService, private ngRedux: NgRedux<AppState>) {}
 
 	getSessions() {
-		return this.apiService.get<Session[]>('http://localhost:1234/api/sessions').subscribe((sessions) => {
+		return this.apiService.get<Session[]>(this.sessionUrl).subscribe((sessions) => {
 			this.ngRedux.dispatch<Action>(createServerSessionsAction(sessions));
 		});
 	}
 
 	saveUserSessions(sessions: Session[]) {
 		this.ngRedux.dispatch<Action>(createUserSessionsAction(sessions));
+	}
+
+	uploadSession(session: Session): Observable<any> {
+		return this.apiService.post(this.sessionUrl, session);
 	}
 }
